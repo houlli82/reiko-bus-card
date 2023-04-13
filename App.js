a9b2ad99453b0fc9c7b4c597586d18879b9b62dc
mark_houlli <markhoulli@protonmail.com>
2023-04-14 05:53:18 +0900

    Contact

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import Logo from "./assets/vivadog_sign.png";
 import Reiko from "./assets/reiko.jpg";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
@@ -14,6 +15,36 @@ export default function App() {
       <View style={styles.about}>
         <Text style={styles.text}>Hello,{"\n"}{"\n"}I'm Reiko Houlli, CEO of VIVA! DOG.{"\n"}{"\n"}VIVA! DOG opened in 2003 with the aim to provide quality pet grooming and boarding.</Text>
         <Image source={Reiko} style={styles.profile} />
+      </View>
+
+      <View style={styles.contact}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => { Linking.openURL(`tel:0426238115`); }}>
+            <Ionicons name="call-outline" style={styles.icon} size={32} />
+          </TouchableOpacity>
+          <Text style={styles.buttonText}>042-623-8115</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => { Linking.openURL(`tel:08010073207`); }}>
+            <Ionicons name="phone-portrait-outline" style={styles.icon} size={32} />
+          </TouchableOpacity>
+          <Text style={styles.buttonText}>080-1007-3207</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => { Linking.openURL('mailto:houlli82@@htomail.co.uk?subject=Enquiries') }}>
+            <Ionicons name="mail-outline" style={styles.icon} size={32} />
+          </TouchableOpacity>
+          <Text style={styles.buttonText}>vivadog2003@gmail.com</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => { Linking.openURL('https://www.google.com/maps/search/?api=1&query=Viva+Dog+Hachioji') }}>
+            <Ionicons name="location-outline" style={styles.icon} size={32} />
+          </TouchableOpacity>
+          <Text style={styles.buttonText}>〒192-0073, Hachioji-shi, Tokyo</Text>
+        </View>
       </View>
 
     </View>
@@ -66,5 +97,33 @@ const styles = StyleSheet.create({
     borderRadius: 130,
     borderColor: 'white',
     borderWidth: 0.3,
+  },
+  contact: {
+    flex: 3,
+    justifyContent: 'space-around',
+    paddingLeft: 12,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: 'white',
+    backgroundColor: 'red',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 16,
+    marginLeft: 12,
+  },
+  icon: {
+    color: 'white',
+    alignSelf: 'center',
   }
 });
