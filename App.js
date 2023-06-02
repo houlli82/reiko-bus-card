dcd24fc04a6a9ba239521ce37a25c82f34412c6e
mark_houlli <markhoulli@protonmail.com>
2023-06-02 18:02:33 +0900

    OG description

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from 'react-native';
+import Head from "expo-router/head";
 import Header from "./Components/Header/Header";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
@@ -6,12 +7,18 @@ import Bottom from "./Components/Bottom/Bottom";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <About />
-      <Contact />
-      <Bottom />
-    </View >
+    <>
+      <Head>
+        <title>My App</title>
+        <meta name="description" content="My App description" />
+      </Head>
+      <View style={styles.container}>
+        <Header />
+        <About />
+        <Contact />
+        <Bottom />
+      </View >
+    </>
   );
 }
 
--- Components/About/styles.js
+++ Components/About/styles.js
@@ -1,30 +1,29 @@
 import { StyleSheet } from 'react-native';
 
 const aboutStyle = StyleSheet.create({
-about: {
-  flex: 1.5,
-  flexDirection: 'row',
-  alignItems: 'center',
-  paddingLeft: 12,
-  paddingRight: 12,
-  paddingTop: 12,
-  paddingBottom: 12,
-  backgroundColor: 'black',
-},
-text: {
-  flex: 1,
-  fontSize: 14,
-  color: 'white',
-  paddingRight: 8,
-  fontFamily: 'monospace',
-},
-profile: {
-  height: 130,
-  width: 130,
-  borderRadius: 130,
-  borderColor: 'white',
-  borderWidth: 2,
-}
+  about: {
+    flex: 1.5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 12,
+    paddingBottom: 12,
+    backgroundColor: 'black',
+  },
+  text: {
+    flex: 1,
+    fontSize: 14,
+    color: 'white',
+    paddingRight: 8,
+  },
+  profile: {
+    height: 130,
+    width: 130,
+    borderRadius: 130,
+    borderColor: 'white',
+    borderWidth: 2,
+  }
 });
 
 export { aboutStyle }
